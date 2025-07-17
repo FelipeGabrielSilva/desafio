e8b63a3a6af29e13771ed495b8440ec8a18e405a
Felipe <contatofelipedev@outlook.com>
2025-07-17 15:46:20 -0300

    feat(repository): criando métodos para funcionário

---
+++ src/main/java/com/agt/desafio/repository/FuncionarioRepository.java
@@ -0,0 +1,14 @@
+package com.agt.desafio.repository;
+
+import com.agt.desafio.entity.Funcionario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface FuncionarioRepository extends JpaRepository<Funcionario, Long> {
+    boolean existsByEmail(String email);
+    boolean existsByCnh(String cnh);
+}
