62a863e8f6fe87598bed31fb18ef3de2784caa33
Felipe <contatofelipedev@outlook.com>
2025-07-20 23:09:31 -0300

    feat(service/controller): implementando exceptions personalizados

--- src/main/java/com/agt/desafio/controller/FuncionarioController.java
+++ src/main/java/com/agt/desafio/controller/FuncionarioController.java
@@ -3,50 +3,47 @@ package com.agt.desafio.controller;
 import com.agt.desafio.dto.CriarFuncionarioDTO;
 import com.agt.desafio.dto.UpdateFuncionarioDTO;
 import com.agt.desafio.entity.Funcionario;
+import com.agt.desafio.errors.ResourceBadRequestException;
+import com.agt.desafio.errors.ResourceNotFoundException;
 import com.agt.desafio.service.FuncionarioService;
 import jakarta.validation.Valid;
-import org.apache.coyote.BadRequestException;
-import org.hibernate.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/funcionario")
 public class FuncionarioController {
-    private final FuncionarioService funcionarioService;
-
-    FuncionarioController(FuncionarioService f) {
-        this.funcionarioService = f;
-    }
+    @Autowired
+    private FuncionarioService funcionarioService;
 
     @PostMapping
-    public ResponseEntity<Funcionario> create(@Valid @RequestBody CriarFuncionarioDTO dto) throws BadRequestException {
-        Funcionario func = funcionarioService.Criar(dto);
-        return ResponseEntity.ok(func);
+    public ResponseEntity<HttpStatus> create(@Valid @RequestBody CriarFuncionarioDTO dto) throws ResourceBadRequestException {
+        funcionarioService.Criar(dto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(HttpStatus.CREATED);
     }
 
     @GetMapping
-    public ResponseEntity<List<Funcionario>> getAll() throws ObjectNotFoundException {
+    public ResponseEntity<List<Funcionario>> getAll() throws ResourceNotFoundException {
         return ResponseEntity.ok(funcionarioService.ListarTodos());
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Funcionario> getOne(@PathVariable("id") Long id) throws ObjectNotFoundException {
+    public ResponseEntity<Funcionario> getOne(@PathVariable("id") Long id) throws ResourceNotFoundException {
         return ResponseEntity.ok(funcionarioService.ListarUm(id));
     }
 
     @PatchMapping("/{id}")
-    public ResponseEntity<Funcionario> patch(@PathVariable("id") Long id, @RequestBody UpdateFuncionarioDTO dto) throws ObjectNotFoundException {
+    public ResponseEntity<Funcionario> patch(@PathVariable("id") Long id, @RequestBody UpdateFuncionarioDTO dto) throws ResourceNotFoundException {
         Funcionario f = funcionarioService.Atualizar(id, dto);
         return ResponseEntity.ok(f);
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<String> delete(@PathVariable("id") Long id) throws ObjectNotFoundException {
+    public ResponseEntity<String> delete(@PathVariable("id") Long id) throws ResourceNotFoundException {
         return ResponseEntity.ok(funcionarioService.Deletar(id));
     }
 }
